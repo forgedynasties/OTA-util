b69b28cd06fba58574ba9274c01258440ffc3254
cd4li <alihassan.pekka@gmail.com>
2025-09-24 12:23:16 +0500

    using platform key

--- Android.mk
+++ Android.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := AB_OTATEST
-LOCAL_CERTIFICATE := testkey     # must match platform.pk8/platform.x509.pem
-LOCAL_PRIVILEGED_MODULE := true   # only with platform key
+LOCAL_CERTIFICATE := platform     
+LOCAL_PRIVILEGED_MODULE := true  
 LOCAL_DEX_PREOPT := false
 LOCAL_PRIVATE_PLATFORM_APIS := true
 
