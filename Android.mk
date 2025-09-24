925ec7e68f0fe7efb340e87f5b19cb9ef4da4b0a
cd4li <alihassan.pekka@gmail.com>
2025-09-24 13:28:54 +0500

    testkey

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := AB_OTATEST
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := testkey
 LOCAL_PRIVILEGED_MODULE := true  
 LOCAL_DEX_PREOPT := false
 LOCAL_PRIVATE_PLATFORM_APIS := true
