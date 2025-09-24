908e218f1a15d57dec1daba265bdd1f74acad448
cd4li <alihassan.pekka@gmail.com>
2025-09-24 12:03:00 +0500

    using test key

--- Android.mk
+++ Android.mk
@@ -1,16 +1,12 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
 LOCAL_PACKAGE_NAME := AB_OTATEST
-LOCAL_CERTIFICATE := platform
-
-#LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_CERTIFICATE := testkey     # must match platform.pk8/platform.x509.pem
+LOCAL_PRIVILEGED_MODULE := true   # only with platform key
 LOCAL_DEX_PREOPT := false
 LOCAL_PRIVATE_PLATFORM_APIS := true
+
 include $(BUILD_PACKAGE)
-
-
